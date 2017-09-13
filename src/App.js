a260e17bd6bfc584f14524f3ecf37e7863dff826
Bayode Aderinola <bayodesegun@gmail.com>
2017-09-13 01:02:38 +0100

    Initial customization with bootstrap & react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Navbar inverse fixedTop collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Bayode Aderinola</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Resume</NavItem>
+              <NavItem eventKey={1} href="#">LinkedIn</NavItem>
+              <NavItem eventKey={2} href="#">GitHub</NavItem>    
+              <NavItem eventKey={2} href="#">Email</NavItem>          
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        
+        <div>
+          <h2><code>Some of my work...</code></h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Navbar inverse fixedBottom>
+          <Nav>
+            <NavItem>
+              <p>Copyright &copy; 2017 by Bayode Aderinola</p>
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem>
+              <div className="App-footer">
+                <p className="App-credits"><code>Made with</code><img src={logo} className="App-logo" alt="logo" /></p>                
+              </div>
+            </NavItem>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
